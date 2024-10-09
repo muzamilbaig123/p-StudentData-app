fa8067d6b4c54981a786f9b00ac71f6164179bca
= <amberoortiz@gmail.com>
2024-10-09 12:32:45 -0700

    todo working...

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import {fireStore}  from "@/firebase/firebaseconfig";
-import { addDoc, collection, deleteDoc, doc, getDoc, orderBy, query, serverTimestamp, updateDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, orderBy, query, serverTimestamp, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react"
 
 type AddTodoFirebaseType = {
+    id? : string,
     title: string,
     details: string,
-    dueDate: string
+    dueDate: string,
 }
 async function addTodoFirebase ({title, details, dueDate}:AddTodoFirebaseType) {
     try{
@@ -16,23 +17,50 @@ async function addTodoFirebase ({title, details, dueDate}:AddTodoFirebaseType) {
         title: title,
         details: details,
         dueDate: dueDate,
-        // createdAt: serverTimestamp()
+        createdAt: serverTimestamp()
     })
-    console.log("Todo Has Been Added =>", docRef.id)
+    console.log("Todo Has Been Added Yes. =>", docRef.id)
+    return true;
 }
 catch(e){
-    console.log("Please Solve This Error " + e);
+    console.error("Please Solve This Error Plz Muzmail Baig " + e);
+    return false;
 }
 
+};
+
+// fetch data in firestore database
+
+async function fetchTodos () {
+    const todoColl = collection(fireStore, "Todos");
+    const querSnanShot = await getDocs(query(todoColl, orderBy("createdAt", "desc")));
+    const todosArr: AddTodoFirebaseType[] = [];
+    querSnanShot.forEach((doc)=>{
+        const todoData = doc.data() as AddTodoFirebaseType;
+        todosArr.push({id: doc.id, ...todoData});
+    });
+    return todosArr;
 }
-addTodoFirebase
+
+// delete todos
+
+async function deleteTodos (todoId:string) {
+    try{
+         console.log("Attemting to delete todo with id => ", todoId)
+         await deleteDoc(doc(fireStore, "Todos", todoId))
+
+    }catch(e){
+        
+    }
+}
+
 
 export default function TodoApp () {
     const [todo, setTodo] = useState("");
 
     return (
         <>
-              <h1>Todo</h1>
+              <h1 className="text-center text-3xl pt-10 pb-10 font-bold underline">Todos</h1>
         </>
     )
 }

5232025eb857080009d107fcfd4399ac8a85418a
= <amberoortiz@gmail.com>
2024-11-07 10:40:11 -0800

    only redux-toolkit add because future extra funcanility addition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Dashboard from "@/components/studentmanage/dashboard";
-import UploadData from "@/components/uploadfile/imageFile";
 
 export default function Home () {
   return (
     <>
        <Dashboard /> 
-      <div className="m-10 textce">
-      <UploadData/>
-      </div>
+     
     </>
   )
 }
